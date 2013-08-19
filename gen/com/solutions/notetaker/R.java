401412852772bec98e00a4d6dfd2ba8252f7a82a
Artem Zinoviev <arzinoviev@gmail.com>
2013-08-19 11:46:33 +0300

    Add new menu item (add node)

--- gen/com/solutions/notetaker/R.java
+++ gen/com/solutions/notetaker/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f080009;
+        public static final int addNoteItem=0x7f080008;
         public static final int dateTextView=0x7f080003;
         public static final int dateValueTextView=0x7f080004;
         public static final int noteEditText=0x7f080006;
@@ -45,6 +46,7 @@ public final class R {
     public static final class string {
         public static final int _=0x7f050007;
         public static final int action_settings=0x7f050001;
+        public static final int add_note=0x7f050009;
         public static final int app_name=0x7f050000;
         public static final int date_=0x7f050004;
         public static final int hello_world=0x7f050002;
--- src/com/solutions/notetaker/ListNotesActivity.java
+++ src/com/solutions/notetaker/ListNotesActivity.java
@@ -7,11 +7,12 @@ import java.util.List;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ListNotesActivity extends Activity {
-
+	
 	private List<Note> notes = new ArrayList<Note>();
 	
 	@Override
@@ -47,4 +48,23 @@ public class ListNotesActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		notes.add(new Note("Added note", "Blah", new Date()));
+		ListView notesListView = (ListView)findViewById(R.id.noteslistView);
+		List<String> values = new ArrayList<String>();
+				
+		for(Note note : notes)
+		{
+			values.add(note.getTitle());
+		}
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1,
+				values);
+		
+		notesListView.setAdapter(adapter);
+		
+		return true;
+	}
+	
 }
