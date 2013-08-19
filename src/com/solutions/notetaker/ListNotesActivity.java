b19aaab2aa7a365b8855a618acb421891f3eedbc
Artem Zinoviev <arzinoviev@gmail.com>
2013-08-19 15:00:21 +0300

    implement edit mode

--- src/com/solutions/notetaker/ListNotesActivity.java
+++ src/com/solutions/notetaker/ListNotesActivity.java
@@ -1,24 +1,54 @@
 package com.solutions.notetaker;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ListNotesActivity extends Activity {
 	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Serializable extra = data.getSerializableExtra("Note");
+		if(extra != null)
+		{
+			Note newNote = (Note)extra;
+			notes.add(newNote);
+			populateList();
+		}
+	}
+
 	private List<Note> notes = new ArrayList<Note>();
+	private ListView notesListView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list_notes);
+		notesListView = (ListView)findViewById(R.id.noteslistView);
+
+		notesListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> adapted, View view, int itemNumber,
+					long id) {
+				 Intent editNoteIntent = new Intent(view.getContext(), EditNoteActivity.class);
+				 editNoteIntent.putExtra("Note", notes.get(itemNumber));
+				 startActivity(editNoteIntent);
+			}
+			
+		});
 		
 		notes.add(new Note("First note", "Bla blah", new Date()));
 		notes.add(new Note("Second note", "Bla blah", new Date()));
@@ -38,14 +68,13 @@ public class ListNotesActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		notes.add(new Note("Added note", "Blah", new Date()));
-		populateList();
+		Intent editNoteIntend = new Intent(this, EditNoteActivity.class);
+		startActivityForResult(editNoteIntend, 1);
 		
 		return true;
 	}
 
 	private void populateList() {
-		ListView notesListView = (ListView)findViewById(R.id.noteslistView);
 		List<String> values = new ArrayList<String>();
 				
 		for(Note note : notes)
